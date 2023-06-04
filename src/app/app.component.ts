410fadad50a6e6646ba398d58f79b9bb933d82a7
Arnold <antaloczi.arnold@gmail.com>
2023-06-04 21:00:12 +0200

    Add some primeNg stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.items = this.items = [
+      {
+        label: 'Kezdőlap',
+        icon: 'pi pi-fw pi-home',
+      },
+      {
+        label: 'Termékek',
+        icon: 'pi pi-fw pi-shopping-cart',
+      },
+      {
+        label: 'Felhasználók',
+        icon: 'pi pi-fw pi-user',
+      },
+      {
+        label: 'Kapcsolat',
+        icon: 'pi pi-fw pi-phone',
+      },
+    ];
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, InputTextModule, MenubarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
